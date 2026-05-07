240d53c558d9cf9ccae71523cd132c334ccf5ec8
Giancarlo Trevisan <g.trevisan@keyvisions.it>
2026-05-07 19:23:08 +0200

    feat: enhance kv-jsonform with dynamic form handling and improved dataset management

--- kv-jsonform/kv-jsonform.js
+++ kv-jsonform/kv-jsonform.js
@@ -5,7 +5,7 @@ class KvJsonForm extends HTMLElement {
 	#onchange = (event) => this.#stringifyDataset(event);
 
 	static get observedAttributes() {
-		return ['name', 'for', 'content-type']; // content-type: application/json | application/xml
+		return ['formid', 'for', 'content-type']; // content-type: application/json | application/xml
 	}
 
 	constructor() {
@@ -22,6 +22,10 @@ class KvJsonForm extends HTMLElement {
 
 		this.#IO();
 		this.#parseDataset(this.dataset);
+
+		const form = document.createElement('form');
+		form.setAttribute('id', this.getAttribute('formid') || 'kv-jsonform');
+		this.replaceChildren(form);
 	}
 
 	#IO(action = 'r') {
@@ -53,56 +57,86 @@ class KvJsonForm extends HTMLElement {
 	}
 
 	#parseDataset(dataset) {
-		document.querySelectorAll(`[form="${this.getAttribute("name")}"]`).forEach(element => {
+		const formName = this.getAttribute('formid') || 'kv-jsonform';
+		document.querySelectorAll(`[form="${formName}"]`).forEach(element => {
 			if (!this.#handlers.has(element)) {
 				element.addEventListener('change', this.#onchange);
 				this.#handlers.set(element, this.#onchange);
 			}
 
-			const name = element.name.replace('*', '');
-			switch (element.tagName) {
-				case 'INPUT':
-					switch (element.type) {
-						case 'checkbox':
-							if (typeof dataset[name] === 'string') {
-								const checks = dataset[name]?.split(',') || [];
-								document.querySelectorAll(`input[form="${this.getAttribute("name")}"][type="checkbox"][name="${name}"]`).forEach(check =>
-									check.checked = (checks.indexOf(check.value) !== -1)
-								);
-							} else {
-								document.querySelectorAll(`input[form="${this.getAttribute("name")}"][type="checkbox"][name="${name}"]`).forEach((check, i) =>
-									check.checked = (dataset[name] ?? 0) & 1 << i ? true : false
-								);
+			const name = element.name;
+
+			// KeyVisions eSite patch
+			const skip = (name[0] == '_' && element.type == 'checkbox') || (name[0] == '*' && element.tagName == 'SELECT');
+
+			if (!skip)
+				switch (element.tagName) {
+					case 'INPUT':
+						switch (element.type) {
+							case 'checkbox':
+								if (typeof dataset[name] === 'string') {
+									const checks = dataset[name]?.split(',') || [];
+									document.querySelectorAll(`input[form="${formName}"][type="checkbox"][name="${name}"]`).forEach(check =>
+										check.checked = (checks.indexOf(check.value) !== -1)
+									);
+								} else {
+									document.querySelectorAll(`input[form="${formName}"][type="checkbox"][name="${name}"]`).forEach((check, i) =>
+										check.checked = (dataset[name] || 0) & 1 << i ? true : false
+									);
+								}
+								break;
+							case 'radio': {
+								const radio = document.querySelector(`input[form="${formName}"][type="radio"][name="${name}"][value="${dataset[name] || ''}"]`);
+								if (radio) radio.checked = true;
+								break;
 							}
-							break;
-						case 'radio': {
-							const radio = document.querySelector(`input[form="${this.getAttribute("name")}"][type="radio"][name="${name}"][value="${dataset[name] ?? ''}"]`);
-							if (radio) radio.checked = true;
-							break;
+							case 'color':
+								element.value = dataset[name] || '#000000';
+								break;
+							default:
+								if (dataset[name])
+									dataset[name] = element.value;
+								else
+									element.value = dataset[name];
 						}
-						case 'color':
-							element.value = dataset[name] ?? '#000000';
-							break;
-						default:
-							element.value = dataset[name] ?? element.value;
+						break;
+					case 'SELECT': {
+						const options = dataset[name]?.split(',') || [];
+						element.querySelectorAll('option').forEach(option =>
+							option.selected = options.indexOf(option.value) === -1 ? false : true
+						);
+						break;
 					}
-					break;
-				case 'SELECT': {
-					const options = dataset[name]?.split(',') || [];
-					element.querySelectorAll('option').forEach(option =>
-						option.selected = options.indexOf(option.value) === -1 ? false : true
-					);
-					break;
+					default:
+						if (dataset[name])
+							dataset[name] = element.value;
+						else
+							element.value = dataset[name];
 				}
-				default:
-					element.value = dataset[name] ?? element.value;
-			}
 		});
+
+		// Call external onchange if defined (property or attribute)
+		if (typeof this.onchange === 'function') {
+			this.onchange({
+				type: 'change',
+				target: this,
+				detail: { dataset: this.dataset }
+			});
+		} else if (this.hasAttribute('onchange')) {
+			// Support inline attribute handler (e.g., onchange="handler(event)")
+			const handler = new Function('event', this.getAttribute('onchange'));
+			handler.call(this, {
+				type: 'change',
+				target: this,
+				detail: { dataset: this.dataset }
+			});
+		}
 	}
 
 	#stringifyDataset(event) {
+		const formName = this.getAttribute('formid') || "kv-jsonform";
 		const element = event.target;
-		const name = element.name.replace('*', '');
+		let name = element.name;
 
 		let value;
 		switch (element.tagName) {
@@ -111,11 +145,11 @@ class KvJsonForm extends HTMLElement {
 					case 'checkbox':
 					case 'radio':
 						value = [];
-						document.querySelectorAll(`[form="${this.getAttribute("name")}"][name="${name}"]`).forEach((box, i) => {
+						document.querySelectorAll(`[form="${formName}"][name="${name}"]`).forEach((box, i) => {
 							if (box.checked && box.hasAttribute('value'))
 								value.push(box.value);
 							else if (box.checked)
-								value[0] = (value[0] ?? 0) | 1 << i;
+								value[0] = (value[0] || 0) | 1 << i;
 						});
 						if (value.length === 1 && typeof value[0] === 'number')
 							value = value[0];
@@ -140,7 +174,19 @@ class KvJsonForm extends HTMLElement {
 			default:
 				value = element.value;
 		}
-		this.dataset[name] = value || null;
+
+		if (name[0] === '*' || name[0] === '_') { // KeyVisions eSite patch
+			name = name.replace(/^[_*]/, '');
+			this.dataset[name] = element.form.elements[name]?.value || null;
+		} else
+			this.dataset[name] = value || null;
+
+		if (this.hasAttribute('onchange'))
+			this.onchange({
+				type: 'change',
+				target: this,
+				detail: { dataset: this.dataset }
+			});
 
 		this.#IO('w');
 	}
